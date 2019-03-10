37e7d9f815fbc058e17bcda75d1dbab332887b42
AfikA2001 <48417404+AfikA2001@users.noreply.github.com>
2019-03-10 21:55:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(NTU0Mzg2MDE3NTI4OTcxMjk4.D2b4vQ.d_oFtoww0sfTRe5AoIRkiG0uGg0);
+client.login(process.env.BOT_TOKEN);
